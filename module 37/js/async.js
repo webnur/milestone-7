fbbcb0d2aad3ae3202cc90f0769e0eb46147f832
webnur <mdnuruli291@gmail.com>
2022-09-05 08:17:57 +0600

    async explore

---
+++ module 37/js/async.js
@@ -0,0 +1,10 @@
+
+function thrid (){
+    console.log('thrid')
+}
+console.log('first');
+console.log('second')
+// thrid()
+setTimeout(() => console.log('third'), 5000)
+console.log('forth')
+console.log('fifth')
