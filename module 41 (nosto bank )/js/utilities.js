7599ef0e7e26af88a0a07047043937d581be8bff
webnur <mdnuruli291@gmail.com>
2022-09-11 08:11:50 +0600

    Fix withdraw and Explore debugging technique

--- module 41 (nosto bank )/js/utilities.js
+++ module 41 (nosto bank )/js/utilities.js
@@ -2,8 +2,9 @@ function getInputFieldValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
   const inputFieldValue = parseFloat(inputFieldValueString);
-  return inputFieldValue;
   inputField.value = "";
+  return inputFieldValue;
+
 }
 
 function getTextElementValueById(elementId) {
@@ -15,5 +16,5 @@ function getTextElementValueById(elementId) {
 
 function setTextElementValueById(elementId, newValue) {
   const textElement = document.getElementById(elementId);
-  textElement.value = newValue;
+  textElement.innerText = newValue;
 }
