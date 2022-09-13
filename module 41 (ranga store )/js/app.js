b7a73bcec48959bcdb52e5225aafd43af5803f1f
webnur <mdnuruli291@gmail.com>
2022-09-13 21:32:46 +0600

    Explore Price rounding to fix product quantity and price

--- module 41 (ranga store )/js/app.js
+++ module 41 (ranga store )/js/app.js
@@ -68,16 +68,16 @@ const showProductDetailsInModal = (product_details) => {
 
 const getInputValue = (id) => {
    const element = document.getElementById(id).innerText;
-   const converted = parseInt(element);
+   const converted = parseFloat(element);
    return converted;
 };
 
 // main price update function
 const updatePrice = (id, value) => {
    const convertedOldPrice = getInputValue(id);
-   const convertPrice = parseInt(value);
+   const convertPrice = parseFloat(value);
    const total = convertedOldPrice + convertPrice;
-   document.getElementById(id).innerText = Math.round(total);
+   document.getElementById(id).innerText = total;
 };
 
 // set innerText function
